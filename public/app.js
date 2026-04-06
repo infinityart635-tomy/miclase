7a9684fad959b043e498e5c3cd7ba736204b3eb1
infinityart635-tomy <infinityart635@gmail.com>
2026-04-06 05:11:56 -0300

    Update MiClase

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-document.getElementById("year").textContent = new Date().getFullYear();
+const year = document.getElementById("year");
+const startButton = document.getElementById("startButton");
+const backButton = document.getElementById("backButton");
+const welcomePanel = document.getElementById("welcomePanel");
+const loginPanel = document.getElementById("loginPanel");
+
+year.textContent = new Date().getFullYear();
+
+startButton.addEventListener("click", () => {
+  welcomePanel.classList.add("hidden");
+  loginPanel.classList.remove("hidden");
+});
+
+backButton.addEventListener("click", () => {
+  loginPanel.classList.add("hidden");
+  welcomePanel.classList.remove("hidden");
+});
